c1bde48a42929b7aeb617773afe62e94a00205ed
yhyem <yoonhyemin0120@gmail.com>
2023-11-11 16:50:37 +0900

    feat: UserList API 연결

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from 'react';
+import { UserPayload } from '../payload/UserPayload';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const baseURL = process.env.REACT_APP_API_URL;
+
 const Users = () => {
-  return <>UserList 페이지</>;
+  const [userList, setUserList] = useState<UserPayload[]>([]);
+  const { data, error, isLoading } = useSWR(`${baseURL}/api/users`, fetcher);
+
+  useEffect(() => {
+    setUserList(data);
+  }, []);
+
+  return <>{userList && userList.map((user) => user)}</>;
 };
 
 export default Users;
